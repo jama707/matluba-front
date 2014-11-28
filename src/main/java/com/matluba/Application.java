ccf153a920440fcbb14ef22145c985ede050aa5a
Jama <jama.bek7@gmail.com>
2014-11-27 16:31:02 -0800

    optimization = excluding persistence configurations

--- src/main/java/com/matluba/Application.java
+++ src/main/java/com/matluba/Application.java
@@ -2,6 +2,8 @@ package com.matluba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +21,7 @@ import javax.servlet.ServletException;
 
 @Configuration
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/matluba/ApplicationWebXml.java
+++ src/main/java/com/matluba/ApplicationWebXml.java
@@ -15,25 +15,8 @@ public class ApplicationWebXml extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.profiles(addDefaultProfile())
+        return application.profiles(DEV)
                 .sources(Application.class);
     }
 
-    /**
-     * Set a default profile if it has not been set.
-     * <p/>
-     * <p>
-     * Please use -Dspring.profiles.active=dev
-     * </p>
-     */
-    private String addDefaultProfile() {
-        String profile = System.getProperty("spring.profiles.active");
-        if (profile != null) {
-            log.info("Running with Spring profile(s) : {}", profile);
-            return profile;
-        }
-
-        log.warn("No Spring profile configured, running with default configuration");
-        return DEV;
-    }
 }
